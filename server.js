5b5aa7e79d6f71205a4cfbcc8690bf859b56826f
张岩 <superrepublic@163.com>
2018-08-20 16:45:11 +0800

    router
    改造createService函数 增加路由参数

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var http = require("http");
-function start(){
-  http.createServer(function(request, response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("nihao");
+var url = require("url");
+
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
     response.end();
-  }).listen(8888);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
-//导出函数
+
 exports.start = start;
