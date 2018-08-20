52de039b9f09631d9989f2415c2071a71bc905da
张岩 <superrepublic@163.com>
2018-08-20 16:59:44 +0800

    requestHandlers
    增加路由的requestHandlers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
 var http = require("http");
 var server = require("./server.js");
-server.start();
+var router = require("./router");
+server.start(router.route);
---
+++ requestHandlers.js
@@ -0,0 +1,8 @@
+function start(){
+    console.log("Request handle 'start' was called");
+}
+function upload(){
+    console.console.log("Request handle 'end' was called");
+}
+exports.start = start;
+exports.upload = upload;
