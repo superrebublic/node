33d5919fcb78db97a85d8ec9f8e65d721933dedc
张岩 <superrepublic@163.com>
2018-08-28 10:53:26 +0800

    express

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
-
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
